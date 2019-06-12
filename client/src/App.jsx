2f11bd34c2a08e4052f84c861a856452f4c304e0
alaabadra <alaabadra4@gmail.com>
2019-06-13 01:14:52 +0300

    create my app without cards only print item until now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,9 @@ import {
   Header,
   Footer,
   PageNotFound,
+  
 } from './Components';
+import MyApplications from './Components/MyApplications';
 
 
 export default class App extends Component{
@@ -29,6 +31,7 @@ export default class App extends Component{
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/home" component={Home} />
+            <Route exact path="/myapps" component={MyApplications} />
             <Route component={PageNotFound} />
           </Switch>
           <Footer />
---
+++ client/src/Components/MyApplications/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import myApplicationsData from '../utils/myApplications';
+
+import './style.css';
+
+export default class MyApplications extends React.Component {
+  state = {
+    myApplications: [],
+  };
+
+  componentDidMount() {
+    // fetch for myApplications data and store it in the state
+    this.setState({ myApplications: myApplicationsData });
+  }
+
+  render() {
+    const { myApplications } = this.state;
+    return (
+      <>
+        <Container className="page__container">
+          <div className="my-applications__title">
+            <span>My Applications</span>
+          </div>
+          {myApplications
+            ? myApplications.map(item => {
+                
+                  console.log(item);
+                  
+                
+              })
+            : null}
+        </Container>
+      </>
+    );
+  }
+}
