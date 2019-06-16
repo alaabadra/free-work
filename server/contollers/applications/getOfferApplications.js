617804c0d17f37efd16ee5804396815bb8e7f2df
alaabadra <alaabadra4@gmail.com>
2019-06-16 21:04:17 +0300

    index.js in appliaction in controller , getOfferApplications in appliactions in controllers , index and getOfferApplications in queries

--- server/contollers/applications/getOfferApplications.js
+++ server/contollers/applications/getOfferApplications.js
@@ -1,4 +1,19 @@
-//in applicants in offerDetails --> when click hire button 
-module.exports = (req,res)=>{
-    res.send('get offer apps')
-}
+const { getOfferApplications } = require('../../database/queries/applications/index');
+console.log('mmmmmmmmmmmmmmmmmmmmmm');
+
+module.exports = (req, res, next) => {
+  const { offerId } = req.params;
+  console.log(offerId);
+  getOfferApplications(offerId)
+  
+  
+    .then((result) => {
+      console.log(result);
+      
+      res.send({
+        error: null,
+        data: result.rows,
+      });
+    })
+    .catch(() => next({ code: 500, msg: 'Internal Server Error!' }));
+};
